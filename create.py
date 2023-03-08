8a874b1dfdd8b5c7ecccbd19549a9814cb658ab3
AIMadeScripts <118119475+AIMadeScripts@users.noreply.github.com>
2023-03-08 14:02:30 +1300

    Update create.py

--- create.py
+++ create.py
@@ -23,16 +23,29 @@ def create_script():
 
     with open(f"{script_name}.py", "w") as f:
         f.write("#!/usr/bin/env python\n\n")
-        f.write("import os\n\n")
+        f.write("###===Import Modules Go Here===###")
+        f.write(f"\n")
+        f.write("import os\n")
+        f.write(f"\n")
+        f.write("###===Separator Function For Pretty Menu===###")
+        f.write(f"\n")
+        f.write(f"def print_separator():\n")
+        f.write("    print(\"=\" * 50)\n\n")
+        f.write(f"###===Your Viewable Menu===###")
+        f.write(f"\n")
         f.write(f"def menu():\n")
         f.write("    clear_screen()\n")
-        f.write(f"    print('{script_name.replace('_', ' ').title()} Menu')\n")
+        f.write(f"    print('{script_name.replace('_', ' ').title()}')\n")
+        f.write("    print_separator()\n")
         for i in range(num_options):
             f.write(f"    print('{i+1}. {options[i].title()}')\n")
         f.write(f"    print('(exit) Exit')\n\n")
+        f.write("###===Making Clear Screen Work On Windows And Linux===###")
         f.write(f"\n")
         f.write(f"def clear_screen():\n")
         f.write("    os.system('cls' if os.name == 'nt' else 'clear')\n\n")
+        f.write("###===When You Choose An Option It Runs One Of These===###")
+        f.write(f"\n")
         for i in range(num_options):
             # Get function name based on option name
             func_name = func_names[i]
@@ -42,6 +55,8 @@ def create_script():
         f.write(f"def exit_menu():\n")
         f.write("    print('Goodbye!')\n")
         f.write("    exit()\n\n")
+        f.write("###===Enter Your Choice And Then It Will Activate That Option===###")
+        f.write(f"\n")
         f.write(f"if __name__ == '__main__':\n")
         f.write("    while True:\n")
         f.write("        menu()\n")
