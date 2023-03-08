7c33e2741f8a42fb39a23db2cd7d57504db65435
AIMadeScripts <118119475+AIMadeScripts@users.noreply.github.com>
2023-03-08 13:07:47 +1300

    Update create.py

--- create.py
+++ create.py
@@ -8,14 +8,18 @@ def create_script():
     script_name = script_name.replace(" ", "_")
     num_options = int(input("How many menu options do you want? "))
     options = []
+    func_names = []
     for i in range(num_options):
         option_name = input(f"Name of option {i+1}: ")
         while option_name == "exit":
             print("Note: 'exit' is already included as an option and does not need to be added.")
             option_name = input(f"Name of option {i+1}: ")
+        func_name = option_name.lower().replace(" ", "_").replace("-", "").replace(".", "")
         options.append(option_name)
+        func_names.append(func_name)
 
     options.append("Exit")
+    func_names.append("exit_menu")
 
     with open(f"{script_name}.py", "w") as f:
         f.write("#!/usr/bin/env python\n\n")
@@ -30,7 +34,9 @@ def create_script():
         f.write(f"def clear_screen():\n")
         f.write("    os.system('cls' if os.name == 'nt' else 'clear')\n\n")
         for i in range(num_options):
-            f.write(f"def option_{i+1}():\n")
+            # Get function name based on option name
+            func_name = func_names[i]
+            f.write(f"def {func_name}():\n")
             f.write(f"    print('You selected {options[i]}')\n")
             f.write("    input('Press enter to return to menu...')\n\n")
         f.write(f"def exit_menu():\n")
@@ -42,7 +48,8 @@ def create_script():
         f.write("        choice = input('Enter your choice: ')\n")
         for i in range(num_options):
             f.write(f"        if choice == '{i+1}':\n")
-            f.write(f"            option_{i+1}()\n")
+            func_name = func_names[i]
+            f.write(f"            {func_name}()\n")
         if "exit" in options:
             f.write("        elif choice.lower() == 'exit' or choice == '0':\n")
         else:
@@ -52,6 +59,7 @@ def create_script():
         f.write("            print('Invalid choice. Try again.')\n")
 
     print(f"Script '{script_name}.py' created successfully!")
+
 
 clear_screen()
 create_script()
